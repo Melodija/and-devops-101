3ba3bbdfe31b67eaa4836da9455314c194fa0395
Ruta Baltiejute <ruta.baltiejute@and.digital>
2020-02-03 15:52:21 +0000

    change develop branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deployed app...
+          Develop Branch
         </a>
       </header>
     </div>
