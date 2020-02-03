591fe24e58a0e507bff9f923ddf5f8ba4e0cb8ea
Ruta Baltiejute <ruta.baltiejute@and.digital>
2020-02-03 15:10:16 +0000

    update add.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Test
+          Trying to make it automatically deploy.
         </p>
         <a
           className="App-link"
